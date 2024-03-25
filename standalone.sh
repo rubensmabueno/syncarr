f9885f3a037bd85e50ce3dd009c23aca2a7b0863
Rubens Minoru Andako Bueno <rubensmabueno@hotmail.com>
2024-03-24 23:50:27 -0300

    paremetize schedule

--- standalone.sh
+++ standalone.sh
@@ -6,10 +6,10 @@ redis-server --daemonize yes
 sleep 30
 
 # Start Celery worker
-celery -A syncarr.worker worker --loglevel=info &
+celery -A syncarr.worker worker --loglevel=info -s /tmp/celerybeat-schedule &
 
 # Start Celery beat
-celery -A syncarr.worker beat --loglevel=info &
+celery -A syncarr.worker beat --loglevel=info -s /tmp/celerybeat-schedule &
 
 # Start Flask application
 python syncarr/manage.py
